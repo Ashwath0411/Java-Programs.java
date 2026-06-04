d81520d1a602ff7b27c4eb2695be633a3f4434ea
Ashwath <ashwath0411@gmail.com>
2026-06-04 09:51:42 +0530

    Create arrayList.java

---
+++ arrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class Appform{
+    static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        System.out.println(fruits);
+    }
+}
